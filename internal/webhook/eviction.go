c049b37675bdc1bda1379c9ef1877fe034b691c7
Paul Miller <pmiller@microsoft.com>
2024-12-07 20:53:57 +0000

    record pod evictions to pod conditions

--- internal/webhook/eviction.go
+++ internal/webhook/eviction.go
@@ -6,6 +6,7 @@ import (
 
 	pdbautoscaler "github.com/paulgmiller/k8s-pdb-autoscaler/api/v1"
 	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	policyv1 "k8s.io/api/policy/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
@@ -41,6 +42,14 @@ func (e *EvictionHandler) Handle(ctx context.Context, req admission.Request) adm
 		return admission.Errored(http.StatusInternalServerError, err)
 	}
 
+	podObj := pod.DeepCopy()
+	updatePodCondition(&podObj.Status, &v1.PodCondition{
+		Type:    v1.DisruptionTarget,
+		Status:  v1.ConditionTrue,
+		Reason:  "EvictionAttempt",
+		Message: "eviction attempt recorded by eviction webhook",
+	})
+
 	// List all PDBWatchers in the namespace. Is this expensive for every eviction are we cacching this list and pdbs?
 	pdbWatcherList := &pdbautoscaler.PDBWatcherList{}
 	err = e.Client.List(ctx, pdbWatcherList, &client.ListOptions{Namespace: req.Namespace})
@@ -58,7 +67,7 @@ func (e *EvictionHandler) Handle(ctx context.Context, req admission.Request) adm
 		err := e.Client.Get(ctx, types.NamespacedName{Name: pdbWatcher.Name, Namespace: pdbWatcher.Namespace}, pdb)
 		if err != nil {
 			logger.Error(err, "Error: Unable to fetch PDB:", "pdbname", pdbWatcher.Name)
-			continue
+			return admission.Errored(http.StatusInternalServerError, err)
 		}
 
 		// Check if the PDB selector matches the evicted pod's labels
@@ -115,4 +124,51 @@ func (e *EvictionHandler) Handle(ctx context.Context, req admission.Request) adm
 func (e *EvictionHandler) InjectDecoder(d *admission.Decoder) error {
 	e.decoder = d
 	return nil
+}
+
+func updatePodCondition(status *v1.PodStatus, condition *v1.PodCondition) bool {
+	condition.LastTransitionTime = metav1.Now()
+	// Try to find this pod condition.
+	conditionIndex, oldCondition := getPodCondition(status, condition.Type)
+
+	if oldCondition == nil {
+		// We are adding new pod condition.
+		status.Conditions = append(status.Conditions, *condition)
+		return true
+	}
+	// We are updating an existing condition, so we need to check if it has changed.
+	if condition.Status == oldCondition.Status {
+		condition.LastTransitionTime = oldCondition.LastTransitionTime
+	}
+
+	isEqual := condition.Status == oldCondition.Status &&
+		condition.Reason == oldCondition.Reason &&
+		condition.Message == oldCondition.Message &&
+		condition.LastProbeTime.Equal(&oldCondition.LastProbeTime) &&
+		condition.LastTransitionTime.Equal(&oldCondition.LastTransitionTime)
+
+	status.Conditions[conditionIndex] = *condition
+	// Return true if one of the fields have changed.
+	return !isEqual
+}
+
+func getPodCondition(status *v1.PodStatus, conditionType v1.PodConditionType) (int, *v1.PodCondition) {
+	if status == nil {
+		return -1, nil
+	}
+	return getPodConditionFromList(status.Conditions, conditionType)
+}
+
+// GetPodConditionFromList extracts the provided condition from the given list of condition and
+// returns the index of the condition and the condition. Returns -1 and nil if the condition is not present.
+func getPodConditionFromList(conditions []v1.PodCondition, conditionType v1.PodConditionType) (int, *v1.PodCondition) {
+	if conditions == nil {
+		return -1, nil
+	}
+	for i := range conditions {
+		if conditions[i].Type == conditionType {
+			return i, &conditions[i]
+		}
+	}
+	return -1, nil
 }
